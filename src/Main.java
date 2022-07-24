85ee5eb76e98aa6bd20d98537e9e95d7b7d3dd81
Joepolymath <joshuaajagbe96@gmail.com>
2022-07-24 18:22:11 +0100

    Importing packages

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Scanner;
+import OOP.Human; // how to import packages in java;;;
+
+//==================================================================================================
 
 public class Main {
     public static void main(String[] args) {
@@ -13,12 +16,13 @@ public class Main {
         int x = 4, y= 6, z = 5; // assigning multiple variables in one line
         int m, n, p;
         m = 10;
-        System.out.println(name.toLowerCase());
-        System.out.println(name2.toUpperCase());
-        System.out.println(x+y+z);
-        System.out.println(m);
-        System.out.println(name.concat(name2));
-        System.out.println(Math.max(x,y));
-        System.out.println(Math.random());
+//        System.out.println(name.toLowerCase());
+//        System.out.println(name2.toUpperCase());
+//        System.out.println(x+y+z);
+//        System.out.println(m);
+//        System.out.println(name.concat(name2));
+//        System.out.println(Math.max(x,y));
+//        System.out.println(Math.random());
+        Human friend = new Human("Joshua", 32, true); //used this to test how to import java packages
     }
 }
--- src/OOP/Human.java
+++ src/OOP/Human.java
@@ -9,7 +9,7 @@ public class Human {
 
     //    access control using encapsulation paradigm
     private String complexionArray[] = {"fair", "dark"};
-    String complexion = complexionArray[0];
+    private String complexion = complexionArray[0];
 
 //    trying out constructors
     public Human(String Name, int Age, boolean IsMale){
@@ -26,5 +26,28 @@ public class Human {
     }
     public void setComplexionArray(String[] colorArray){
         this.complexionArray = colorArray;
+    }
+
+    public void setComplexion(String color){
+        boolean exists = false;
+        color = color.toLowerCase();
+        System.out.println(color);
+        for(int i = 0; i<this.complexionArray.length; i++){
+            if(color == complexionArray[i]){
+                exists = true;
+                break;
+            } else {
+                exists = false;
+            }
+        }
+        if(exists){
+            complexion = color;
+            System.out.println("Complexion set to " + color);
+        }else{
+            System.out.println("Color not in complexion list.");
+        }
+    }
+    public void getComplexion(){
+        System.out.println(complexion);
     }
 }
