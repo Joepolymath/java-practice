357ea8ea9df582ce1535d25c40b40a97a9e2e6ad
Joepolymath <joshuaajagbe96@gmail.com>
2022-07-25 22:31:37 +0100

    private inner classes

--- src/OOP/OuterClass.java
+++ src/OOP/OuterClass.java
@@ -5,11 +5,20 @@ public class OuterClass {
     class InnerClass{
         String message = "This is the inner class";
     }
+
+//    having a static class implies that you can access it without creating an object of the outer class.
+    static class StaticClass{
+        String staticMessage = "I am in the static class";
+}
+
     public static void main(String[] args){
         OuterClass obj1 = new OuterClass();
         System.out.println(obj1.outerMessage);
 
         OuterClass.InnerClass obj2 = obj1.new InnerClass();
         System.out.println(obj2.message);
+
+        StaticClass obj3 = new StaticClass();
+        System.out.println(obj3.staticMessage);
     }
 }
