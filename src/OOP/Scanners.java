213203dae43bf4e76687157e89108bf3d271236f
Joepolymath <joshuaajagbe96@gmail.com>
2022-07-27 00:03:22 +0100

    Scanners

---
+++ src/OOP/Scanners.java
@@ -0,0 +1,12 @@
+package OOP;
+
+import java.util.Scanner;
+
+public class Scanners {
+
+    public static void main(String[] argzz){
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println(name);
+    }
+}
